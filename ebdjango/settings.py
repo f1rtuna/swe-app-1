bba2b40cdba9a4f0e8c900d463dca841b280c033
Kil-Hwan <kilhwanpyun@gmail.com>
2022-10-20 02:16:42 -0400

    re-deployed

--- ebdjango/settings.py
+++ ebdjango/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '0^^yon=b7w0@871p#z2nldkv8c%(5ri73&pi21yhq%-lqrkxdp'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['django-env1.eba-vycj2xkm.us-west-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['django-env1.eba-vycj2xkm.us-west-2.elasticbeanstalk.com', '127.0.0.1']
 
 
 # Application definition
